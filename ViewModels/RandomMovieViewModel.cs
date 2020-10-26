1711616dbfcd04c364cfd764dc6f4845453e24f6
giuseppe <giuseppe.ranieri@outlook.it>
2020-10-26 14:00:47 +0100

    View Models, lesson 15

---
+++ ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+
+    //a view model is a model specifically build for a view
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
