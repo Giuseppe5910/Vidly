ea5c6bbbba12222d34f84b518877320fa007c240
giuseppe <giuseppe.ranieri@outlook.it>
2020-10-26 19:08:53 +0100

    Complete Logic MVC

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer> 
+            {
+                new Customer{Id=1,Name="peppe"},
+                new Customer{Id=2,Name="ciccio" }
+            };
+        }
+    }
+}
